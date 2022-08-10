6c1082e8e70aeb7a88ff5fce35024f9657b9dd8c
Noa Resare <noa.resare@gs.com>
2022-08-10 12:46:13 +0100

    Create helper record Result, with content assertion (#42)

--- src/test/java/com/gs/crdtools/codegen/CustomGenerationTest.java
+++ src/test/java/com/gs/crdtools/codegen/CustomGenerationTest.java
@@ -5,6 +5,7 @@ import com.gs.crdtools.SourceGenFromSpec;
 import io.vavr.collection.List;
 import io.vavr.collection.HashSet;
 import io.vavr.collection.Map;
+import io.vavr.collection.Stream;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -28,16 +29,28 @@ public class CustomGenerationTest {
 
         var p = Path.of(runFiles.rlocation("__main__/src/test/resources/minimal-openapi.yaml"));
         var crd = List.of(new SourceGenFromSpec.Spec("", "", Files.readString(p)));
-        var result = SourceGenFromSpec.generateSourceCodeFromSpecs(crd);
+        var result = new Result(SourceGenFromSpec.generateSourceCodeFromSpecs(crd));
 
-        assertEquals(HashSet.of(OUTPUT_FILE), result.keySet());
-        assertIn(result,"GS annotation goes here");
-        assertIn(result, "import com.gs.crdtools.BaseObject");
+        assertEquals(HashSet.of(OUTPUT_FILE), result.inner().keySet());
+
+        result.assertIn("Thing.java","GS annotation goes here");
+        result.assertIn("Thing.java", "import com.gs.crdtools.BaseObject");
     }
 
     private void assertIn(Map<Path, String> result, String in) {
         assertTrue(
                 result.get(OUTPUT_FILE).get().contains(in),
                 "Output file does not contain '%s'".formatted(in));
+    }
+
+    public record Result(Map<Path, String> inner) {
+        public void assertIn(String file, String in) {
+
+            Path key = Stream.ofAll(inner.keySet()).find(candidate -> candidate.endsWith(file)).getOrElseThrow(IllegalArgumentException::new);
+            assertTrue(
+                    inner.get(key).get().contains(in),
+                    "Output file does not contain '%s'".formatted(in));
+
+        }
     }
 }
