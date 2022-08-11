024a6b1c63ce424b107ee11600205317975fde26
Alfredo Musumeci <alfredo.msmeci@gmail.com>
2022-08-11 15:46:04 +0100

    delete old references to k8sconfigconnector

--- src/main/java/com/gs/crdtools/SpecExtractorHelper.java
+++ src/main/java/com/gs/crdtools/SpecExtractorHelper.java
@@ -6,7 +6,6 @@ import com.resare.nryaml.YAMLValue;
 import io.vavr.Tuple;
 import io.vavr.collection.HashMap;
 import io.vavr.collection.List;
-import org.yaml.snakeyaml.Yaml;
 
 /**
  * A helper class to extract the openAPI specs from a given yaml file in the format of a string.
@@ -75,29 +74,4 @@ public class SpecExtractorHelper {
          return HashMap.of("type", qualifiedType).toJavaMap();
     }
 
-    /**
-     * Get the crds yaml file from the previously downloaded repository.
-     * This only works if the k8s-config-connector has been successfully downloaded by bazel.
-     * @return A list reflecting the content of the crds.yaml file.
-     * @throws IllegalArgumentException If the file cannot be read or the list is empty.
-     */
-    static List<Object> getCrdsYaml() throws IllegalArgumentException {
-        List<Object> allTheYamls;
-
-        try {
-            allTheYamls = List.ofAll(new Yaml().loadAll(SpecExtractorHelper.class.getResourceAsStream(
-                    "/external/k8s-config-connector/install-bundles/install-bundle-workload-identity/crds.yaml")));
-        } catch (Exception ex) {
-            // Correct exception would be IOException, but it is thrown at runtime;
-            // hence not recognised by the try block
-            throw new IllegalArgumentException("Error occurred while accessing the yaml file. " +
-                    "Has the repo been download correctly?", ex);
-        }
-
-        if (allTheYamls.isEmpty()) {
-            throw new IllegalArgumentException("Didn't find any crds?");
-        }
-
-        return allTheYamls;
-    }
 }
